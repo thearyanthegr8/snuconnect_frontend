44a5aed09a5e59d2bf3a3cdf03046aa39ccf7da3
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-06-21 09:52:39 +0530

    navbar underline fix

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return (
+        <div className="min-h-screen flex flex-col items-center">
+            <div className="h-[20vh]"></div>
+            <div className="">Meet our Team of Creators, Designers, and Problem Solvers!</div>
+        </div>
+    )
+}
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,23 +1,46 @@
+"use client";
+
 import Image from "next/image";
 import logo from "../../../public/images/logo.svg";
-import { Button } from "../ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
   return (
-    <div className="absolute py-4 w-full flex justify-between px-20 z-[1000] bg-white shadow-md">
+    <div className="absolute py-4 w-full flex justify-between px-12 z-[1000] bg-white shadow-md">
       <Image src={logo} alt="SNU-Connect" />
-      <div className="flex font-bold gap-16">
-        <Link className="my-auto h-fit text-lg" href="/">
+      <div className="flex font-bold justify-evenly w-1/3">
+        <Link
+          className={`my-auto h-fit text-lg underline-offset-4 decoration-2 decoration-[#0054CF] ${
+            pathName == "/" ? "underline" : ""
+          }`}
+          href="/"
+        >
           Home
         </Link>
-        <Link className="my-auto h-fit text-lg" href="/routes">
+        <Link
+          className={`my-auto h-fit text-lg underline-offset-4 decoration-2 decoration-[#0054CF] ${
+            pathName == "/routes" ? "underline" : ""
+          }`}
+          href="/routes"
+        >
           Routes
         </Link>
-        <Link className="my-auto h-fit text-lg" href="/maps">
+        <Link
+          className={`my-auto h-fit text-lg underline-offset-4 decoration-2 decoration-[#0054CF] ${
+            pathName == "/maps" ? "underline" : ""
+          }`}
+          href="/maps"
+        >
           Maps
         </Link>
-        <Link className="my-auto h-fit text-lg" href="/about">
+        <Link
+          className={`my-auto h-fit text-lg underline-offset-4 decoration-2 decoration-[#0054CF] ${
+            pathName == "/about" ? "underline" : ""
+          }`}
+          href="/about"
+        >
           About
         </Link>
       </div>
