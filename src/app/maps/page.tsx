d86718e67887d5b6a2249b2feae9cae95dcb0194
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-11-13 14:58:15 +0530

    Fix: Build error

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 import React from "react";
-import Map from "@/components/_map";
 import ETADisplay from "@/components/_eta-display";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
 export default function Page() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/_map"), {
+        loading: () => <span>Loading...</span>,
+        ssr: false,
+      }),
+    []
+  );
   if (typeof window === "undefined") return null;
   else {
     return (
-      <section className="w-full h-screen bg-red-100 relative">
+      <section className="w-full relative">
         <Map />
+        <ETADisplay />
       </section>
     );
   }
